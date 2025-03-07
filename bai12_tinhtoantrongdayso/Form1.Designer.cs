f485b890b7b2265e53852e72a935d1146e31cf1f
SPC <SPC@19-LAB402>
2025-03-07 10:44:53 +0700

    Add project files.

---
+++ bai12_tinhtoantrongdayso/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace bai12_tinhtoantrongdayso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btTinh = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.txtChan = new System.Windows.Forms.TextBox();
+            this.txtLe = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btTinh
+            // 
+            this.btTinh.Location = new System.Drawing.Point(196, 163);
+            this.btTinh.Name = "btTinh";
+            this.btTinh.Size = new System.Drawing.Size(160, 56);
+            this.btTinh.TabIndex = 0;
+            this.btTinh.Text = "tính";
+            this.btTinh.UseVisualStyleBackColor = true;
+            this.btTinh.Click += new System.EventHandler(this.btTinh_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "số đầu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "số cuối";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(280, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "kết quả";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "tổng số chắn";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(280, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "tổng số lẻ";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(150, 48);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 20);
+            this.txtMin.TabIndex = 2;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(150, 82);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 20);
+            this.txtMax.TabIndex = 2;
+            // 
+            // txtChan
+            // 
+            this.txtChan.Location = new System.Drawing.Point(355, 41);
+            this.txtChan.Name = "txtChan";
+            this.txtChan.Size = new System.Drawing.Size(100, 20);
+            this.txtChan.TabIndex = 2;
+            this.txtChan.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtLe
+            // 
+            this.txtLe.Location = new System.Drawing.Point(355, 78);
+            this.txtLe.Name = "txtLe";
+            this.txtLe.Size = new System.Drawing.Size(100, 20);
+            this.txtLe.TabIndex = 2;
+            this.txtLe.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(355, 119);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(100, 20);
+            this.txtKQ.TabIndex = 2;
+            this.txtKQ.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtLe);
+            this.Controls.Add(this.txtChan);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btTinh);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btTinh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.TextBox txtChan;
+        private System.Windows.Forms.TextBox txtLe;
+        private System.Windows.Forms.TextBox txtKQ;
+    }
+}
+
---
+++ bai12_tinhtoantrongdayso/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai12_tinhtoantrongdayso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btTinh_Click(object sender, EventArgs e)
+        {
+            int min = Convert.ToInt32(txtMin.Text);
+            int max = Convert.ToInt32(txtMax.Text);
+            int chan = 0; int le = 0;
+
+            for (int i = min; i <= max; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+
+                    chan += i;
+                }
+                else
+                {
+                    le += i;
+
+                }
+
+            }
+        }
+
+    }
+}
